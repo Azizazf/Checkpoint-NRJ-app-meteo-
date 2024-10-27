d541677e26f5c871fce54bdf8210df4112be4dac
Azizazf <fayeabdou13@gmail.com>
2024-10-27 16:34:01 +0000

    Checkpoint NRJ app meteo

--- appmeteo/script.js
+++ appmeteo/script.js
@@ -10,26 +10,22 @@ document.getElementById('getWeather').addEventListener('click', async function (
     }
 });
 
-
-
 const fetchWeatherData = async (city) => {
     const apiKeys = "4cc18094901b09475d5bc95205a5b006";  // déclaration de la clé API
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKeys}&units=metric`; // URL de l'API
 
     try {
-        const response = await fetch(url);$
-        const data = await response.json();
-        if (response.ok) {
+        const response = await fetch(url);
+        if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-       // const data = await response.json();
+        const data = await response.json();
         displayWeather(data);
     } catch (error) {
         console.error('Fetch error:', error);
         alert('Erreur lors de la récupération des données météo');
     }
 };
-
 
 function displayWeather(data) {
     const { name } = data;
@@ -44,7 +40,6 @@ function displayWeather(data) {
     document.getElementById('wind').textContent = 'Vitesse du vent: ' + speed + ' km/h';
     document.getElementById('icon').src = `http://openweathermap.org/img/w/${icon}.png`;
 }
-
 
 // Charger les données météo pour Paris au chargement de la page
 window.onload = () => {
